c386e8d3f26d6a7c4c5db8a804324c573c76bc1a
Michael Huang <mwhuang2@gmail.com>
2015-11-18 21:35:31 -0600

    Ionic Bitcoin app

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,48 @@
+angular.module('App', ['ionic'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: '/tabs',
+      abstract: true,
+      templateUrl: 'views/tabs/tabs.html'
+    })
+    .state('tabs.rates', {
+      url: '/rates',
+      views: {
+        'rates-tab': {
+          templateUrl: 'views/rates/rates.html'
+        }
+      }
+    })
+    .state('tabs.history', {
+      url: '/history',
+      views: {
+        'history-tab': {
+          templateUrl: 'views/history/history.html'
+        }
+      }
+    })
+    .state('tabs.currencies', {
+      url: '/currencies',
+      views: {
+        'currencies-tab': {
+          templateUrl: 'views/currencies/currencies.html'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/tabs/rates');
+})
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
